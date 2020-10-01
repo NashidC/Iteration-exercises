3a918d70d9a8a45072253c745dc417ae839c7c12
NashidC <nash.chowdhury@gmail.com>
2020-09-30 21:25:16 -0400

    lab completed

--- main.js
+++ main.js
@@ -2,10 +2,26 @@
 //Using map, write a function that takes an array of numbers
 //and makes them strings
 
+function arrConvert(arr) {
+  let newArr = arr.map(String)
+  return newArr;
+}
+console.log(arrConvert([1, 2, 3, 4]))
+
 //Exercise #2
 //You must use forEach for this exercise
 //Write a function that accepts an array and returns a new array
 //with all the values in the array passed to the function doubled
+
+function doubleArr(arrr) {
+  let newArrr = [];
+  arrr.forEach(function (item) {
+    newArrr.push(item * 2);
+  });
+  return newArrr;
+}
+
+console.log(doubleArr([1, 2, 3, 4]));
 
 //Exercise #3
 //Write a function called firstAndLast which accepts
@@ -14,6 +30,21 @@
 //Example: ['winning', 'tomato'] => ['wg', 'to']
 //hint: forEach show be used here
 
+function firstAndLast(strArray) {
+  let newArray = [];
+  strArray.forEach(function (currentString) {
+    if (typeof currentString === "string") {
+      newArray.push(currentString[0] + currentString[currentString.length - 1]);
+    }
+  });
+  return newArray;
+}
+console.log(firstAndLast(['winning', 'tomato']))
+
 //Bonus:
 //Write a for loop that outputs this:
 // 1 1 1 2 2 2 3 3 3 4 4 4
+for (let i = 1; i <= 4; i++) {
+  for (let g = 0; g < 3; g++)
+    console.log(i);
+}
